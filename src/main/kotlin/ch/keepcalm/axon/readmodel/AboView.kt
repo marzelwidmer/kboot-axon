ab5d65d35089040734d49df54115ee5057f23fc4
Marcel Widmer <marzelwidmer@gmail.com>
2021-12-17 08:54:50 +0100

    Work on configuration

--- src/main/kotlin/ch/keepcalm/axon/readmodel/AboView.kt
+++ src/main/kotlin/ch/keepcalm/axon/readmodel/AboView.kt
@@ -1,20 +1,8 @@
 package ch.keepcalm.axon.readmodel
 
-import org.springframework.data.jpa.repository.JpaRepository
 import java.time.LocalDate
 import java.util.*
 import javax.persistence.*
-//
-//@Entity
-//class AboView(
-//    @Id val aboId : UUID? = null,
-//    val eMail: String,
-//    val startDatum: LocalDate,
-//    var endDatum: LocalDate? = null,
-//    var recipe: String? = null
-//) //: AbstractEntity()
-//
-//
 
 @Entity
 class AboView(
