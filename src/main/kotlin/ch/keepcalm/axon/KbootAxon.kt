8a44bd1a197024c7d52ec53aef1c9489e5cc9944
Marcel Widmer <marzelwidmer@gmail.com>
2022-02-08 07:27:44 +0100

    Configure SecureXStreamSerializer

--- src/main/kotlin/ch/keepcalm/axon/KbootAxon.kt
+++ src/main/kotlin/ch/keepcalm/axon/KbootAxon.kt
@@ -1,41 +1,66 @@
 package ch.keepcalm.axon
 
 import com.mongodb.client.MongoClient
+import com.thoughtworks.xstream.XStream
 import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine
 import org.axonframework.eventsourcing.eventstore.EventStore
 import org.axonframework.extensions.mongo.DefaultMongoTemplate
 import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoEventStorageEngine
+import org.axonframework.serialization.xml.XStreamSerializer
 import org.axonframework.spring.config.AxonConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Profile
-import java.util.*
 
 
 @SpringBootApplication
 class KbootAxon {
+    @Bean
+    fun eventStore(storageEngine: EventStorageEngine?, configuration: AxonConfiguration): EmbeddedEventStore? {
+        return EmbeddedEventStore.builder()
+            .storageEngine(storageEngine)
+            .messageMonitor(configuration.messageMonitor(EventStore::class.java, "eventStore"))
+            .build()
+    }
+    // The `MongoEventStorageEngine` stores each event in a separate MongoDB document
+    @Bean
+    fun storageEngine(client: MongoClient?): EventStorageEngine? {
+        return MongoEventStorageEngine.builder()
+            .eventSerializer(
+                XStreamSerializer.builder()
+                    .xStream(SecureXStreamSerializer.xStream())
+                    .build()
 
-//	@Profile("mongo")
-	@Bean
-	fun eventStore(storageEngine: EventStorageEngine?, configuration: AxonConfiguration): EmbeddedEventStore? {
-		return EmbeddedEventStore.builder()
-			.storageEngine(storageEngine)
-			.messageMonitor(configuration.messageMonitor(EventStore::class.java, "eventStore"))
-			.build()
-	}
-
-	// The `MongoEventStorageEngine` stores each event in a separate MongoDB document
-//	@Profile("mongo")
-	@Bean
-	fun storageEngine(client: MongoClient?): EventStorageEngine? {
-		return MongoEventStorageEngine.builder().mongoTemplate(DefaultMongoTemplate.builder().mongoDatabase(client).build()).build()
-	}
-
+            )
+            .snapshotSerializer(
+                XStreamSerializer.builder()
+                    .xStream(SecureXStreamSerializer.xStream())
+                    .build()
+            )
+            .mongoTemplate(
+                DefaultMongoTemplate
+                    .builder()
+                    .mongoDatabase(client)
+                    .build()
+            ).build()
+    }
+}
+object SecureXStreamSerializer {
+    fun xStream(): XStream {
+        val xStream = XStream()
+        xStream.classLoader = SecureXStreamSerializer::class.java.classLoader
+        xStream.allowTypesByWildcard(
+            arrayOf(
+                "org.axonframework.**",
+                "**",
+            )
+        )
+        return XStreamSerializer.builder().xStream(xStream).build().xStream
+    }
 }
 
 fun main(args: Array<String>) {
-	runApplication<KbootAxon>(*args)
-	println(UUID.randomUUID())
+    runApplication<KbootAxon>(*args)
 }
+
